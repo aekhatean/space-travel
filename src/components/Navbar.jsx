40679b1ce1297afc2f83477e027a1534a2e000e2
Adham Khatean <aekhatean@gmail.com>
2021-11-19 01:59:54 +0200

    Create responsive nav bar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,49 @@ import React from "react";
 import logo from "../assets/shared/logo.svg";
 
 export default function Navbar() {
+  /**
+   * handle the action of clicking the nav menu icon
+   * by opening the nav menu if it was closed
+   * or close it if it was open
+   *
+   * @param clickEvent
+   */
+  const toggleMobNavSlide = (clickEvent) => {
+    const navMenu = clickEvent.target.nextSibling.firstChild;
+    const navMenuButton = clickEvent.target;
+    const visibility = navMenu.getAttribute("data-visible");
+
+    if (visibility === "false") {
+      navMenu.setAttribute("data-visible", true);
+      navMenuButton.setAttribute("aria-expanded", true);
+    } else {
+      navMenu.setAttribute("data-visible", false);
+      navMenuButton.setAttribute("aria-expanded", false);
+    }
+  };
+
   return (
     <header className="primary-header flex">
       <div>
-        <img src={logo} alt="space-travel logo" />
+        <img src={logo} alt="space-travel logo" className="logo" />
       </div>
-      {/* <nav>
-        <ul className="primary-navigation underline-indicators flex">
+      <button
+        className="mobile-nav-toggle"
+        aria-controls="primary-navigation"
+        aria-expanded="false"
+        data-toggle="burger"
+        onClick={(event) => {
+          toggleMobNavSlide(event);
+        }}
+      >
+        <span className="sr-only">Menu</span>
+      </button>
+      <nav>
+        <ul
+          id="primary-navigation"
+          className="primary-navigation underline-indicators flex"
+          data-visible="false"
+        >
           <li className="active">
             <a
               className="ff-sans-cond uppercase letter-spacing-2 txt-white"
@@ -48,7 +84,7 @@ export default function Navbar() {
             </a>
           </li>
         </ul>
-      </nav> */}
+      </nav>
     </header>
   );
 }
