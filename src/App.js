c1c892a35f18487113da8b08bf3f6d3e07ffd049
Adham Khatean <aekhatean@gmail.com>
2021-11-19 16:59:02 +0200

    Fix home background limitations.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 // Import Components
 import Navbar from "./components/Navbar";
@@ -9,8 +9,9 @@ import Home from "./pages/Home";
 import DesignSystem from "./pages/DesignSystem";
 
 function App() {
+  const homepage = useLocation().pathname === "/" ? "homepage-bg" : "";
   return (
-    <div className="App">
+    <div className={`App ${homepage}`}>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
